51daac19ae15b18fbf623b7a7f25e9960ae81a5c
Luka <code.coding2001@gmail.com>
2023-10-17 23:03:16 +0400

    here we go again

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,11 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        String word="";
-        int num = 0;
-        while(num < chars.length){
-            word+=chars[num];
+        int index = 0;
 
+        while (index < chars.length) {
+            System.out.print(chars[index]);
+            index++;
         }
     }
 }
